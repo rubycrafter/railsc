e3bdd3042e1f417475b9041c32ce6c7e7ff89a30
rubycrafter <you@example.com>
2016-04-03 15:03:18 +0500

    Clear locomotive csm with two wagons

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '17b3a20ffaae74d1f95ea192ae98205d215ac5a99e4158c9366c03c5f42b3576'
+
+  url_format '/images/dynamic/:job/:sha/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
